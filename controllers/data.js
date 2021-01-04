2f9e0291e7aba8e86aa394eedcc48b06c26c6ad5
victory-a <victoryasokomeh@gmail.com>
2021-01-04 13:26:38 +0100

    get banks and utility enpoints done

--- controllers/data.js
+++ controllers/data.js
@@ -1,2 +1,19 @@
 const ErrorResponse = require("../utils/errorResponse");
 const { asyncHandler } = require("../middleware");
+const { gotv, dstv, kwese, banks } = require("../data");
+
+exports.fetchBanks = asyncHandler(async (req, res, next) => {
+  return res.status(200).json({ data: banks });
+});
+
+exports.gotvPlans = asyncHandler(async (req, res, next) => {
+  return res.status(200).json({ data: gotv });
+});
+
+exports.dstvPlans = asyncHandler(async (req, res, next) => {
+  return res.status(200).json({ data: dstv });
+});
+
+exports.kwesePlans = asyncHandler(async (req, res, next) => {
+  return res.status(200).json({ data: kwese });
+});
